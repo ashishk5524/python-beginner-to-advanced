7662b2f0b31ba99779a80ab763dc2d193e23cca0
ASHISH KANDEKAR <103416975+ashishk5524@users.noreply.github.com>
2025-01-01 00:20:30 +0530

    Create kmtomiles.py

---
+++ section1/kmtomiles.py
@@ -0,0 +1,6 @@
+#WAP for km to miles converter
+km=float(input("type no.of kilometers:"))
+miles= km/1.609344
+print(km,"km=",round(miles,4),"miles")
+#o/p=type no.of kilometers:100
+#100.0 km= 62.13711922373339 miles
