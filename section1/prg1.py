bce3e33f5e4fb42fe148f4df5df794e7f49395ec
ASHISH KANDEKAR <103416975+ashishk5524@users.noreply.github.com>
2025-01-01 00:19:29 +0530

    Create prg1.py

---
+++ section1/prg1.py
@@ -0,0 +1,14 @@
+print("Calculate avg of two numbers")
+number1=float(input("enter 1st number"))
+number2=float(input("enter 2nd number"))
+print("numbers are",number1, "and", number2)
+print("avg is:",(number1+number2)/2) #o/p=5.0
+
+#make prg dynamic so we use variables
+#variable-used to store variables
+
+#rules
+#can only contain letters numbers and underscores
+#cant start with numbers
+#case sensitive -age not equal to Age
+
