4c5160c562ae0e949f1a3d7bfb733ded02e16aa5
ASHISH KANDEKAR <103416975+ashishk5524@users.noreply.github.com>
2025-01-01 00:21:52 +0530

    Create prg3.py

---
+++ section2/prg3.py
@@ -0,0 +1,26 @@
+myString= "Some text"
+print(type(myString))
+
+myString="33"
+print(type(myString))
+
+myString="She said meet me at 5'"
+print(myString[0])
+
+print(myString[-1])
+print(len(myString))
+
+myString="she"
+print(len(myString))
+
+print(myString[len(myString)-1])
+
+#slicing
+myString="PT10000"
+print(myString[0:2])
+print(myString[-2:-1])
+
+print(myString[-5:])
+print(myString[:3])
+
+print("user"+str("234"))
